4fb171b00b790c70261bb3807692c835a4e63931
hhhbx <972916730@qq.com>
2024-12-26 22:55:04 +0800

    feat(docs): 初始化核心模块

---
+++ docs/.vuepress/plume.config.ts
@@ -0,0 +1,52 @@
+import path from 'node:path'
+import { defineThemeConfig } from 'vuepress-theme-plume'
+import { enNavbar, zhNavbar } from './navbar.js'
+import { enNotes, zhNotes } from './notes/index.js'
+
+export default defineThemeConfig({
+  logo: '/plume.png',
+
+  profile: {
+    avatar: '/plume.png',
+    name: 'Plume Theme',
+    description: 'The Theme for Vuepress 2.0',
+    location: 'GuangZhou, China',
+    organization: 'pengzhanbo',
+  },
+
+  social: [
+    { icon: 'github', link: 'https://github.com/pengzhanbo/vuepress-theme-plume' },
+    { icon: 'qq', link: 'https://qm.qq.com/q/FbPPoOIscE' },
+  ],
+  navbarSocialInclude: ['github', 'qq'],
+
+  footer: {
+    copyright: 'Copyright © 2021-present pengzhanbo',
+  },
+
+  locales: {
+    '/': {
+      notes: zhNotes,
+      navbar: zhNavbar,
+    },
+    '/en/': {
+      notes: enNotes,
+      navbar: enNavbar,
+    },
+  },
+
+  encrypt: {
+    rules: {
+      '/article/enx7c9s/': '123456',
+    },
+  },
+  autoFrontmatter: { exclude: ['**/*.snippet.*'] },
+
+  bulletin: {
+    layout: 'top-right',
+    lifetime: 'always',
+    // title: '🎉 公告 🎉',
+    contentFile: path.join(__dirname, 'bulletin.md'),
+    enablePage: page => page.path === '/guide/features/bulletin/',
+  },
+})
---
+++ docs/.vuepress/theme.ts
@@ -0,0 +1,69 @@
+import type { Theme } from 'vuepress'
+import process from 'node:process'
+import { plumeTheme } from 'vuepress-theme-plume'
+
+export const theme: Theme = plumeTheme({
+  hostname: process.env.SITE_HOST || 'https://plume.pengzhanbo.cn',
+  docsRepo: 'https://github.com/pengzhanbo/vuepress-theme-plume',
+  docsDir: 'docs',
+  changelog: { maxCount: 10 },
+  contributors: { mode: 'block' },
+
+  plugins: {
+
+    shiki: {
+      twoslash: true,
+      lineNumbers: 10,
+      languages: ['sh', 'ts', 'md', 'html', 'js', 'go', 'kotlin', 'rust', 'vue', 'css', 'json', 'scss', 'yaml', 'bash', 'c++', 'java', 'py', 'ruby', 'make', 'objc', 'swift', 'php', 'rs', 'sql', 'xml', 'zig', 'pug', 'http', 'less', 'styl', 'jsx', 'tsx', 'astro', 'svelte', 'wasm', 'vb', 'bat', 'cs', 'cpp', 'mermaid'],
+    },
+
+    markdownEnhance: {
+      demo: true,
+      chartjs: true,
+      echarts: true,
+      mermaid: true,
+      flowchart: true,
+    },
+    markdownPower: {
+      imageSize: 'all',
+      pdf: true,
+      caniuse: true,
+      bilibili: true,
+      youtube: true,
+      artPlayer: true,
+      audioReader: true,
+      codepen: true,
+      replit: true,
+      codeSandbox: true,
+      jsfiddle: true,
+      npmTo: ['pnpm', 'yarn', 'npm'],
+      repl: {
+        go: true,
+        rust: true,
+        kotlin: true,
+      },
+    },
+
+    comment: {
+      provider: 'Giscus',
+      comment: true,
+      repo: 'pengzhanbo/vuepress-theme-plume',
+      repoId: 'R_kgDOG_ebNA',
+      category: 'docs-comment',
+      categoryId: 'DIC_kwDOG_ebNM4Cd0uF',
+      mapping: 'pathname',
+      reactionsEnabled: true,
+      inputPosition: 'top',
+      darkTheme: 'dark_protanopia',
+      lightTheme: 'light_protanopia',
+    },
+
+    watermark: {
+      enabled: false,
+      watermarkOptions: {
+        content: 'vuepress-theme-plume',
+      },
+    },
+
+  },
+})
